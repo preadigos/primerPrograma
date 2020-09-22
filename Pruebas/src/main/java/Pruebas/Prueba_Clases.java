3814e1669a76056d77ab79a9cb30c4aaa42f61a6
preadigos <preadigos@yahoo.es>
2020-09-21 22:19:47 -0300

    Modificación de pruebas

--- Pruebas/src/main/java/Pruebas/Prueba_Clases.java
+++ Pruebas/src/main/java/Pruebas/Prueba_Clases.java
@@ -2,7 +2,7 @@
 package Pruebas;
 
 public class Prueba_Clases {
-    public static void main(String[] argv){
+    public static void main(String argv[]){
         String holaMundo="Hola Mundo Java";
         System.out.println(holaMundo);
     }
